5075a8f8e5b2b5c71b2abe37820c7c41a69f5763
juned <1132210708@mitwpu.edu.in>
2023-06-13 12:51:55 +0530

    ;

--- Auth_app.py
+++ Auth_app.py
@@ -24,103 +24,103 @@ username = fetched_credentials[3]
 password = fetched_credentials[4]
 tablename = fetched_credentials[5]
 
-# logger = Logger(log_file, server, database, username, password, tablename)
+logger = Logger(log_file, server, database, username, password, tablename)
 
 
-# class User:
-#     def __init__(self, username, password):
-#         self.username = username
-#         self.password = password
+class User:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
-#     @staticmethod
-#     def register(users):
-#         start_time = time.time()
-#         username = input("Enter a username: ")
-#         password = input("Enter a password: ")
+    @staticmethod
+    def register(users):
+        start_time = time.time()
+        username = input("Enter a username: ")
+        password = input("Enter a password: ")
 
-#         if not username.strip() or not password.strip():
-#             logger.log_message("error", "Blank username or password submitted")
-#             print("Error: Username and password cannot be blank.")
-#             return
+        if not username.strip() or not password.strip():
+            logger.log_message("error", "Blank username or password submitted")
+            print("Error: Username and password cannot be blank.")
+            return
 
-#         logger.log_message("debug", "Registering user: {}".format(username))
+        logger.log_message("debug", "Registering user: {}".format(username))
 
-#         if not re.match(r"[^@]+@gmail\.com$", username):
-#             logger.log_message(
-#                 "warning", "Invalid username format: {}".format(username))
-#             print("Invalid username! Please enter a Gmail address.")
-#             return
+        if not re.match(r"[^@]+@gmail\.com$", username):
+            logger.log_message(
+                "warning", "Invalid username format: {}".format(username))
+            print("Invalid username! Please enter a Gmail address.")
+            return
 
-#         if username[0] in "!@#$%^&*()-+?_" or password[0] in "!@#$%^&*()-+?_":
-#             logger.log_message(
-#                 "warning", "Invalid username or password format")
-#             print(
-#                 "Invalid username or password! They should not start with a special character.")
-#             return
+        if username[0] in "!@#$%^&*()-+?_" or password[0] in "!@#$%^&*()-+?_":
+            logger.log_message(
+                "warning", "Invalid username or password format")
+            print(
+                "Invalid username or password! They should not start with a special character.")
+            return
 
-#         user = User(username, password)
-#         users.append(user)
-#         end_time = time.time()
-#         elapsed_time = end_time - start_time
-#         logger.log_message("info", "User registered: {}. Time taken: {} seconds".format(
-#             username, elapsed_time))
-#         logger.log_message("debug", "Registration process ended")
-#         print("Registration successful!")
+        user = User(username, password)
+        users.append(user)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        logger.log_message("info", "User registered: {}. Time taken: {} seconds".format(
+            username, elapsed_time))
+        logger.log_message("debug", "Registration process ended")
+        print("Registration successful!")
 
-#     @staticmethod
-#     def login(users):
-#         start_time = time.time()
+    @staticmethod
+    def login(users):
+        start_time = time.time()
 
-#         username = input("Enter your username: ")
-#         password = input("Enter your password: ")
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
 
-#         if not username.strip() or not password.strip():
-#             logger.log_message("error", "Blank username or password submitted")
-#             print("Error: Username and password cannot be blank.")
-#             return
+        if not username.strip() or not password.strip():
+            logger.log_message("error", "Blank username or password submitted")
+            print("Error: Username and password cannot be blank.")
+            return
 
-#         logger.log_message("debug", "Logging in user: {}".format(username))
+        logger.log_message("debug", "Logging in user: {}".format(username))
 
-#         for user in users:
-#             if user.username == username and user.password == password:
-#                 end_time = time.time()
-#                 elapsed_time = end_time - start_time
-#                 logger.log_message("info", "User logged in: {}. Time taken: {} seconds".format(
-#                     username, elapsed_time))
-#                 logger.log_message("debug", "Login process ended")
-#                 print("Login successful!")
-#                 return
+        for user in users:
+            if user.username == username and user.password == password:
+                end_time = time.time()
+                elapsed_time = end_time - start_time
+                logger.log_message("info", "User logged in: {}. Time taken: {} seconds".format(
+                    username, elapsed_time))
+                logger.log_message("debug", "Login process ended")
+                print("Login successful!")
+                return
 
-#         logger.log_message(
-#             "warning", "Invalid login attempt: {}".format(username))
-#         logger.log_message("debug", "Login process ended")
-#         print("Invalid username or password.")
-
-
-# def main():
-#     users = []
-#     while True:
-#         print("\nUser Authentication System")
-#         print("1. Register")
-#         print("2. Login")
-#         print("3. Exit")
-
-#         choice = input("Enter your choice (1-3): ")
-
-#         if choice == "1":
-#             logger.log_message("debug", "Registration process started")
-#             User.register(users)
-#         elif choice == "2":
-#             logger.log_message("debug", "Login process started")
-#             User.login(users)
-#         elif choice == "3":
-#             print("Exiting the program...")
-#             logger.log_message("info", "Program exited")
-#             break
-#         else:
-#             logger.log_message("warning", "Invalid choice: {}".format(choice))
-#             print("Invalid choice! Please try again.")
+        logger.log_message(
+            "warning", "Invalid login attempt: {}".format(username))
+        logger.log_message("debug", "Login process ended")
+        print("Invalid username or password.")
 
 
-# if __name__ == "__main__":
-#     main()
+def main():
+    users = []
+    while True:
+        print("\nUser Authentication System")
+        print("1. Register")
+        print("2. Login")
+        print("3. Exit")
+
+        choice = input("Enter your choice (1-3): ")
+
+        if choice == "1":
+            logger.log_message("debug", "Registration process started")
+            User.register(users)
+        elif choice == "2":
+            logger.log_message("debug", "Login process started")
+            User.login(users)
+        elif choice == "3":
+            print("Exiting the program...")
+            logger.log_message("info", "Program exited")
+            break
+        else:
+            logger.log_message("warning", "Invalid choice: {}".format(choice))
+            print("Invalid choice! Please try again.")
+
+
+if __name__ == "__main__":
+    main()
